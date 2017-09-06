6a986895b9337a540b96a278abac28c5c8f39afd
SamanthaSterner <samanthalsterner@gmail.com>
2017-09-06 12:31:24 -0400

    Coped stuff

--- GettingStarted/Program.cs
+++ GettingStarted/Program.cs
@@ -10,7 +10,29 @@ namespace GettingStarted
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            for (int i = 1; i <= 3; i++)
+            {
+
+                Console.WriteLine("Input a username: ");
+                String entereduse = Console.ReadLine();
+                Console.WriteLine("Input a password: ");
+                String enteredpass = Console.ReadLine();
+
+                if ("root" == entereduse)
+                {
+                    if ("letmein" == enteredpass)
+                    {
+                        Console.WriteLine("Username and password authenticated... you may proceed");
+                    }
+                }
+                else
+                {
+                    if (i == 3)
+                    {
+                        Console.WriteLine("Too many incorrect login attemps. try again later");
+                    }
+                }
+            }
         }
     }
 }
