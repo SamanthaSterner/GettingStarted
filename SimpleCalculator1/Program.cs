f820b2e20b74a7b14227fada71f9f0ea6b84e8bb
SamanthaSterner <samanthalsterner@gmail.com>
2017-09-08 11:06:08 -0400

    Simple Calculator

---
+++ SimpleCalculator1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCalculator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an expression: ");
+            String Input = Console.ReadLine();
+            String[] parts = Input.Split(' ');
+
+
+            int firstnum = Convert.ToInt32(parts[0]);
+            int secondnum = Convert.ToInt32(parts[2]);
+            String op = parts[1];
+            int answer = 0;
+
+            if(op == "+")
+            {
+                answer = firstnum + secondnum;
+
+            }
+
+            if (op == "-")
+            {
+                answer = firstnum - secondnum;
+
+            }
+
+            if (op == "*")
+            {
+                answer = firstnum * secondnum;
+
+            }
+
+            if (op == "/")
+            {
+                answer = firstnum / secondnum;
+
+            }
+
+
+
+            Console.Write(answer);
+        }
+    }
+}
