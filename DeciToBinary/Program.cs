963f4f341cb348feb56084308e675be253d1d8e2
SamanthaSterner <samanthalsterner@gmail.com>
2017-09-14 12:07:41 -0400

    deci to binary and palindrome

---
+++ DeciToBinary/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeciToBinary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.Write("Enter a Number : ");
+            num = int.Parse(Console.ReadLine());
+            int quot;
+
+            string rem = "";
+
+            while (num >= 1)
+            {
+                quot = num / 2;
+                rem += (num % 2).ToString();
+                num = quot;
+            }
+
+            // Reversing the  value
+            string bin = "";
+            for (int i = rem.Length - 1; i >= 0; i--)
+            {
+                bin = bin + rem[i];
+
+            }
+
+            Console.WriteLine("Result: {0}", bin);
+        }
+    }
+}
---
+++ GettingStarted/Palindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Phrase: ");
+            String phrase = Console.ReadLine();
+            char[] reverse = phrase.ToCharArray();
+            reverse.Reverse();
+            String pal = new String(reverse);
+            if (pal == phrase)
+            {
+                Console.Write("\"" + phrase + "\" is a palindrome.");
+            }
+             else
+            {
+                Console.Write("\"" + phrase + "\" is not a palindrome.");
+            }
+        }
+    }
+}
