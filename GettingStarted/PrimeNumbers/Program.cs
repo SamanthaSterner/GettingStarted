4846e156a9609f2c3bb54da583d16c303373fd09
SamanthaSterner <samanthalsterner@gmail.com>
2017-09-15 12:17:31 -0400

    Primes

--- GettingStarted/PrimeNumbers/Program.cs
+++ GettingStarted/PrimeNumbers/Program.cs
@@ -8,8 +8,44 @@ namespace PrimeNumbers
 {
     class Program
     {
+        static bool isPrime(List<int> primes, int n)
+        {
+            // Find one example where n isn't prime
+
+            // Check every integer in primes to see if n is evenly divisible by it
+            foreach (int p in primes)
+            {
+                if (n % p == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+
         static void Main(string[] args)
         {
+            List<int> primes = new List<int>();
+            primes.Add(2); // slot 1
+            primes.Add(3); // slot 2
+            Console.Write(" 2, 3, ");
+            int count = 2, n = 5; // n is numbers being tested
+            while (count < 1000)
+            {
+                if (isPrime(primes, n))
+                {
+                    
+                    primes.Add(n); // adds number to list if prime
+                    Console.Write(n.ToString().PadLeft(4) + ", ");
+                    count++;
+                }
+
+                n += 2;
+            }
+            
         }
     }
 }
+
